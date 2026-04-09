9e3592924f8b446720786e4456b1d36ee3285625
황원상 <benny@inswave.com>
2026-04-09 11:06:52 +0900

    container/lineshape/rect 처리해야함

--- HwpxParser/HwpxParser/Models/Components/Paragraph/Paragraph.m
+++ HwpxParser/HwpxParser/Models/Components/Paragraph/Paragraph.m
@@ -103,28 +103,34 @@ extern HH_Head *head;
                 NSInteger next = 0;
                 NSUInteger charStack = 0;
                 int startIdx = 0;
+                HTMLElement *pendingElement = nil; // 이전 줄에서 분할된 나머지 텍스트
                 for (int i = 0; i < [cutPoints count];i++) {
-                    
+
                     if (i == [cutPoints count] - 1) {
                         // 마지막
                         next = NSIntegerMax;
                     } else {
                         next = [cutPoints[i+1] integerValue];
                     }
-                    
+
                     Lineseg *lineSeg = linesegarray.lineseg[i];
                     // wraapper innerDiv
                     WrapperP *wrapper = [lineSeg getOuterP];
                     HTMLElement *innerDiv = wrapper.inner;
-                    
+
                     HTMLElement *line = [[HTMLElement alloc] initWithTagName:@"span" attributes:[@{
                         @"style" : @"display:flex"
                     }mutableCopy]];
-                    
-                    // Contentsw
+
+                    // 이전 줄에서 분할된 나머지가 있으면 현재 줄 앞에 배치
+                    if (pendingElement) {
+                        [line appendNode:pendingElement];
+                        pendingElement = nil;
+                    }
+
+                    // Contents
                     for (;startIdx < [self.run count]; startIdx++) {
                         Run *targetRun = self.run[startIdx];
-                        Text *targetText = targetRun.text;
                         if ([targetRun.contents count] == 0) {
                             continue;
                         }
@@ -140,20 +146,46 @@ extern HH_Head *head;
                                 @"style" : charStyle
                             }mutableCopy]];
                             [line appendNode:content];
+                        } else if (charStack == next) {
+                            // 현재 run의 텍스트가 정확히 줄바꿈 지점에서 끝남
+                            // -> 현재 line에 추가하고 다음 lineseg로 이동
+                            HTMLElement *content = [targetRun getContent].outer;
+                            NSDictionary *charPr = [head getCharPr:targetRun.charPrIDRef];
+                            NSMutableString *charStyle = [NSMutableString string];
+                            for (NSString *key in charPr) {
+                                [charStyle appendFormat:@" %@:%@;", key, charPr[key]];
+                            }
+                            [content setAttributes:[@{
+                                @"style" : charStyle
+                            }mutableCopy]];
+                            [line appendNode:content];
+                            startIdx++;
+                            break;
                         } else {
+                            // charStack > next: 텍스트가 줄바꿈 지점을 넘어감
+                            // -> 텍스트를 분할하여 앞부분은 현재 줄, 뒷부분은 다음 줄로
                             NSDictionary *charPr = [head getCharPr:targetRun.charPrIDRef];
                             NSMutableString *charStyle = [NSMutableString string];
                             for (NSString *key in charPr) {
                                 [charStyle appendFormat:@" %@:%@;", key, charPr[key]];
                             }
                             NSUInteger gap = charStack - next;
-                            NSMutableArray *splitTexts = [targetText splitTextAt:gap css:charStyle];
+                            NSMutableArray *splitTexts = [targetRun.text splitTextAt:gap css:charStyle];
+                            // 앞부분 → 현재 줄
+                            [line appendNode:splitTexts[0]];
+                            // 뒷부분 → 다음 줄에 배치될 pending
+                            pendingElement = splitTexts[1];
+                            startIdx++;
+                            break;
                         }
                     }
                     [innerDiv appendNode:line];
                     // result에 append
                     [result addObject:wrapper];
                 }
+            } else {
+                NSLog(@"No Outer Paragraph!!!! make it work");
+                __builtin_trap();
             }
         } else {
             NSLog(@"outer Paragraph logic must be modified!!");
